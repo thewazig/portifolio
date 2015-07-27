02037d8dc7915be5cbca05053d53853d76dc821f
thewazig <thewazig@terra.com.br>
2015-07-27 17:04:50 -0300

    git projeto jade/sass
    XD

--- Assets/js/functions.js
+++ Assets/js/functions.js
@@ -1,5 +1,25 @@
 $(document).ready(function(){
 
-//
+	$(function(){
+
+		smoothScroll(400);
+	});
+
+	//Smooth scroll
+
+	function smoothScroll(duration){
+		$('a[href^="#"]').on('click', function(event){
+
+			var target = $( $(this).attr('href') );
+
+			if( target.length ){
+				event.preventDefault();
+				$('html, body').animate({
+					scrollTop: target.offset().top
+					}, duration);
+			}
+
+		});
+	}
 
 });
